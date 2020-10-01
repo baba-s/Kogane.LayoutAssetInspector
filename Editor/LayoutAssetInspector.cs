5da0b9bb2139d5be4cd45aaed6bfeb2bf658decf
baba_s <shotababa1206@gmail.com>
2020-10-01 13:00:32 +0900

    first commit

---
+++ Editor/LayoutAssetInspector.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[CustomEditor( typeof( DefaultAsset ) )]
+	internal sealed class LayoutAssetInspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			var defaultAsset = ( DefaultAsset ) target;
+			var assetPath    = AssetDatabase.GetAssetPath( defaultAsset );
+			var extension    = Path.GetExtension( assetPath );
+
+			if ( !extension.EndsWith( ".wlt" ) ) return;
+
+			var oldEnabled = GUI.enabled;
+			GUI.enabled = true;
+
+			if ( GUILayout.Button( "Load" ) )
+			{
+				var assembly   = typeof( Editor ).Assembly;
+				var type       = assembly.GetType( "UnityEditor.WindowLayout" );
+				var methodInfo = type.GetMethod( "LoadWindowLayout", new[] { typeof( string ), typeof( bool ) } );
+
+				var fullPath = Path.GetFullPath( assetPath );
+
+				methodInfo.Invoke( null, new object[] { fullPath, false } );
+			}
+
+			GUI.enabled = oldEnabled;
+		}
+	}
+}
